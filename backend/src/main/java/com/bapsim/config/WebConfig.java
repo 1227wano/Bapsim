aad27d45ab7ea2a4a9c489c3b3130a241e423069
iSEViper <jklas187@naver.com>
2025-08-28 19:01:04 +0900

    CORS 문제 해결

---
+++ backend/src/main/java/com/bapsim/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.bapsim.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    // 개발 환경
+                    "http://localhost:8081",     // 프론트엔드 개발 서버
+                    "http://127.0.0.1:8081",     // localhost 대체 주소
+
+                    //AWS 환경
+                    "https://bapsim.site",
+                    "https://www.bapsim.site"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600); // 1시간
+    }
+}
